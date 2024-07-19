1a1b0fd8c36c1f0e3ff0ced750c722a711d2286b
Bhaskar Jha <jhabhaskar1205@gmail.com>
2024-07-18 20:09:54 -0500

    added paddle movement

--- entities.py
+++ entities.py
@@ -1,9 +1,10 @@
 import pygame
+pygame.init()
 
 class Ball():
 
     def __init__(self, color, size):
-        self.color = color
+        self.color = (0, 0, 0)
         self.size = size
 
     def draw(self, screen, x, y):
@@ -12,10 +13,20 @@ class Ball():
 
 class Paddle():
 
-    def __init__(self, color, width, height):
+    def __init__(self, color, x, y, width, height):
         self.color = color
+        self.x = x
+        self.y = y
         self.width = width
         self.height = height
     
-    def draw(self, screen, x, y):
-        pygame.draw.rect(surface=screen, color=self.color, rect=[x, y, self.width, self.height])
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, [self.x, self.y, self.width, self.height])
+
+    def move_up(self, speed):
+        if self.y > 0:
+            self.y -= speed
+    
+    def move_down(self, speed):
+        if self.y < 600 - self.height:
+            self.y += speed
--- main.py
+++ main.py
@@ -7,37 +7,40 @@ class Game:
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode((600, 600))
-
         self.clock = pygame.time.Clock()
 
+        self.paddle1 = entities.Paddle((255, 255, 255), 10, 200, 10, 100)
+                                        #color           x   y  width height
+        self.paddle2 = entities.Paddle((255, 255, 255), 580, 200, 10, 100) 
 
     def run(self):
-        global ball
-        ball = entities.Ball((255, 0, 0), 10)
-
-        global paddle1
-        paddle1 = entities.Paddle((0, 255, 0), 10, 100)
-
-        global paddle2
-        paddle2 = entities.Paddle((0, 0, 255), 10, 100)
-        
-
         while True:
-
-            ball.draw(self.window, 300, 300)
-
-            paddle1.draw(self.window, 10, 300)
-            paddle2.draw(self.window, 590, 300)
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
+            pressed = pygame.key.get_pressed()
+            #Paddle 1 movement
+            if pressed[pygame.K_w]:
+                self.paddle1.move_up(5)
+            if pressed[pygame.K_s]:
+                self.paddle1.move_down(5)
+            #Paddle 2 movement
+            if pressed[pygame.K_UP]:
+                self.paddle2.move_up(5)
+            if pressed[pygame.K_DOWN]:
+                self.paddle2.move_down(5)
+
+
+            # Reset screen to black
+            self.window.fill((0, 0, 0))
+
+            # Update entities
+            self.paddle1.draw(self.window)
+            self.paddle2.draw(self.window)
+
             pygame.display.update()
             self.clock.tick(60)
 
-
 Game().run()
-
-
