6e15659e298fb7b013110aa61beab758f116c7ac
Bhaskar Jha <jhabhaskar1205@gmail.com>
2024-07-19 20:55:54 -0500

    collision detection with ball and paddle

--- entities.py
+++ entities.py
@@ -3,13 +3,33 @@ pygame.init()
 
 class Ball():
 
-    def __init__(self, color, size):
-        self.color = (0, 0, 0)
+    def __init__(self, color, x, y, size):
+        self.color = color
+        self.x = x
+        self.y = y
         self.size = size
 
-    def draw(self, screen, x, y):
-        pygame.draw.circle(surface=screen, color=self.color, center=[x, y], radius=self.size)
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, [self.x, self.y], self.size)
 
+    def bounce(self):
+        pass
+    
+    def move_up(self, speed):
+        if self.y > 0:
+            self.y -= speed
+    
+    def move_down(self, speed):
+        if self.y < 600:
+            self.y += speed
+
+    def move_left(self, speed):
+        if self.x > 0:
+            self.x -= speed
+        
+    def move_right(self, speed):
+        if self.x < 600:
+            self.x += speed
 
 class Paddle():
 
--- main.py
+++ main.py
@@ -11,7 +11,9 @@ class Game:
 
         self.paddle1 = entities.Paddle((255, 255, 255), 10, 200, 10, 100)
                                         #color           x   y  width height
-        self.paddle2 = entities.Paddle((255, 255, 255), 580, 200, 10, 100) 
+        self.paddle2 = entities.Paddle((255, 255, 255), 580, 200, 10, 100)
+
+        self.ball = entities.Ball((255, 255, 255), 300, 300, 10)
 
     def run(self):
         while True:
@@ -32,6 +34,23 @@ class Game:
             if pressed[pygame.K_DOWN]:
                 self.paddle2.move_down(5)
 
+            # Collision detection
+            if self.ball.x - (self.paddle1.x + self.paddle1.width) <= self.ball.size:
+                # check if ball is hitting top or bottom of paddle 1
+                if self.ball.y < self.paddle1.y + (self.paddle1.height / 2):
+                    print("collision with top half of paddle 1")
+                if self.ball.y > self.paddle1.y + (self.paddle1.height / 2):
+                    print("collision with bottom half of paddle 1")
+
+            #ball movement
+            if pressed[pygame.K_i]:
+                self.ball.move_up(5)
+            if pressed[pygame.K_k]:
+                self.ball.move_down(5)
+            if pressed[pygame.K_j]:
+                self.ball.move_left(5)
+            if pressed[pygame.K_l]:
+                self.ball.move_right(5)
 
             # Reset screen to black
             self.window.fill((0, 0, 0))
@@ -39,6 +58,8 @@ class Game:
             # Update entities
             self.paddle1.draw(self.window)
             self.paddle2.draw(self.window)
+
+            self.ball.draw(self.window)
 
             pygame.display.update()
             self.clock.tick(60)
